86cccc1196045484128bf7e6a7e19b167bd6ebf0
chris98 <christophermarshall98@gmail.com>
2015-02-18 17:35:13 +0000

    Create admin_merge.php
    Added language file for AP_User_Merge.php

---
+++ lang/English/admin_merge.php
@@ -0,0 +1,19 @@
+<?php
+
+// Language definitions used in admin_merge.php
+$lang_admin_merge = array(
+
+'no merge user from'	=>	'You must select a user to merge from.',
+'no merge user to'	=>	'You must select a user to merge to.',
+'merge users same'	=>	'You must select two different users.',
+'users merged redirect'	=>	'Users merged successfully. Redirecting …',
+'confirm merge 2'			=>	'Confirm Merge (step 2 of 3)',
+'confirm merge 1'			=>	'User Selection (Step 1 of 3)',
+'merge legend'						=>	'All content by this user on the forum will be given to the other user, and then this user will be deleted.',
+'merge legend 2'			=>	'User to receive the merge',
+'merge help'			=>	'This user will receive all content from the user above. After selection, there will be a confirmation page to ensure everything is correct.',
+'continue'				=>	'Continue to Confirmation Page',
+'user merge legend'		=>	'User to merge',
+'merge message'			=>	'Please do not press the submit button multiple times. This process may take a few minutes.',
+'merge submit'			=>	'Merge Users',
+);
